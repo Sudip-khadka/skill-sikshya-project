c0cf3d806ba8cbdbe73d8df8c51bcbaa40548603
Sudip-khadka <ksudip0123@gmail.com>
2024-06-06 13:36:04 +0545

    made final adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,24 @@ function App() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const handleSmoothScroll = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+
+
   return (
     <>
       <Navbar />
       <div className="home-container">
         <Home />
         <div className="back-to-top">
-          <a href="#nav"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#FF785A" className="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+          <a href="#nav" onClick={handleSmoothScroll}><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#FF785A" className="bi bi-arrow-up-circle" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
           </svg></a>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,33 +33,56 @@ function Navbar() {
     setShowNav(!showNav);
   };
 
+  const handleSmoothScroll = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <nav id="nav">
       <div className="logo">
         <img src={Logo} alt="logo" />
       </div>
       <div className={`middle ${showNav ? 'show' : ''}`} id="middle">
-        <a href="#home">Home</a>
-        <a href="#aboutus">About Us</a>
-        <a href="#faq">FAQ</a>
-        <a href="#contact">Contact Us</a>
+        <a href="#home" onClick={handleSmoothScroll}>Home</a>
+        <a href="#aboutus" onClick={handleSmoothScroll}>About Us</a>
+        <a href="#faq" onClick={handleSmoothScroll}>FAQ</a>
+        <a href="#contact" onClick={handleSmoothScroll}>Contact Us</a>
       </div>
       <div className="right">
         <button type="button">Download Now</button>
         <div className="nav-responsive" id="nav-responsive" onClick={toggleMenu}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            className="bi bi-list"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-            />
-          </svg>
+          {showNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="#082D4A"
+              className="bi bi-x-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              fill="#082D4A"
+              className="bi bi-list"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+              />
+            </svg>
+          )}
         </div>
       </div>
     </nav>
